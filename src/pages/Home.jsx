d0e71a5dca9ae4ff6182a235847d41d878542445
Austin Morrow <morrow.austin5@gmail.com>
2023-03-13 13:36:45 -0600

    wip - light/dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+//import LightLogo from '../assets/brand/light-logo.png'
+import DarkLogo from '../assets/brand/dark-logo.png'
 
 
 const Home = () => {
   return (
-<>
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-</>
+<section className='min-h-screen bg-white_base dark:bg-black_base duration-100'>
+<div>
+  <div className='flex items-center'>
+   <img src={DarkLogo} alt='Austin Morrow' className='w-auto h-24 dark:inverted' />
+   <div className='dark:text-white_base'>Austin Morrow</div>
+  </div>
+  <div className='fixed top-5 right-10 duration-10 dark:bg-slate-800 bg-gray-100 rounded'>
+    <button className='w-8 h-8 leading-9 text-xl rounded-full m-1 text-sky-600'>
+    <span class="material-symbols-outlined">
+light_mode
+</span>
+    </button>
+  </div>
+</div>
+
+</section>
 
     )
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'white_base': '#FFFFFF',
+      'black_base': '#262626',
+
+      // Light mode
+      'gray_light': '#D9D9D9',
+      'orange_light': '#FF9A3D',
+      'blue_light': '#42A4FF',
+
+      //Dark mode
+      'gray_dark': '#858181',
+      'orange_dark': '#C16F23',
+      'blue_dark': '#2567A3'
+    },
+
+
     extend: {},
   },
   plugins: [],
